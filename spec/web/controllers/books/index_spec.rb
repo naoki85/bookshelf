9a8d741abc470659ba60528ebdfdcbb9a2b955ad
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2017-09-11 15:59:30 +0900

    modeling with entities

--- spec/web/controllers/books/index_spec.rb
+++ spec/web/controllers/books/index_spec.rb
@@ -2,11 +2,26 @@ require 'spec_helper'
 require_relative '../../../../apps/web/controllers/books/index'
 
 describe Web::Controllers::Books::Index do
+  # actionにBooks#Indexのインスタンスをセット
   let(:action) { Web::Controllers::Books::Index.new }
+  # パラメーター（今回は何もない）
   let(:params) { Hash[] }
+  # BookRepositoryをセット
+  let(:repository) { BookRepository.new }
 
-  it 'is successful' do
+  before do
+    repository.clear
+
+    @book = repository.create(title: 'TDD', author: 'Kent Beck')
+  end
+
+  it 'ステータスコードの200番が返る' do
     response = action.call(params)
     response[0].must_equal 200
+  end
+
+  it '全てのbooksがexposureによって渡されている' do
+    action.call(params)
+    action.exposures[:books].must_equal [@book]
   end
 end
--- spec/web/views/books/index_spec.rb
+++ spec/web/views/books/index_spec.rb
@@ -2,15 +2,38 @@ require 'spec_helper'
 require_relative '../../../../apps/web/views/books/index'
 
 describe Web::Views::Books::Index do
-  let(:exposures) { Hash[foo: 'bar'] }
+  # コントローラーからbooksの空配列が渡されると仮定
+  let(:exposures) { Hash[books: []] }
+  # 表示するテンプレートを指定
   let(:template)  { Hanami::View::Template.new('apps/web/templates/books/index.html.erb') }
+  # Web::Views::Books::Indexの第一引数で表示するtemplate、第二引数にコントローラーから渡される値
   let(:view)      { Web::Views::Books::Index.new(template, exposures) }
+  # Web::Views::Books::Indexの第一引数で表示するtemplate、第二引数にコントローラーから渡される値
   let(:rendered)  { view.render }
+  # 上のビューインスタンスをレンダリング
+  it 'view.booksとコントローラーから渡されたbooksが等しいことを確認' do
+    view.books.must_equal exposures.fetch(:books)
+  end
 
-  it 'exposes #foo' do
-    skip 'This is an auto-generated test. Edit it and add your own tests.'
+  describe 'booksが空の場合' do
+    it 'プレースホルダーメッセージが表示される' do
+      rendered.must_include('<p class="placeholder">There are no books yet.</p>')
+    end
+  end
 
-    # Example
-    view.foo.must_equal exposures.fetch(:foo)
+  describe 'booksに値がある場合' do
+    let(:book1)     { Book.new(title: 'Refactoring', author: 'Martin Fowler') }
+    let(:book2)     { Book.new(title: 'Domain Driven Design', author: 'Eric Evans') }
+    let(:exposures) { Hash[books: [book1, book2]] }
+
+    it '全て表示される' do
+      rendered.scan(/class="book"/).count.must_equal 2
+      rendered.must_include('Refactoring')
+      rendered.must_include('Domain Driven Design')
+    end
+
+    it 'プレースホルダーメッセージは表示されない' do
+      rendered.wont_include('<p class="placeholder">There are no books yet.</p>')
+    end
   end
 end
